f491de9660c9eae05c7d6d6d8a7213b03865c9e8
paulofranklins <paulofranklinc@gmail.com>
2026-04-02 12:50:21 -0700

    Add GameConfig to wire dependencies without Spring annotations in domain

---
+++ src/main/java/com/pcunha/svt/infrastructure/web/config/GameConfig.java
@@ -0,0 +1,48 @@
+package com.pcunha.svt.infrastructure.web.config;
+
+import com.pcunha.svt.application.*;
+import com.pcunha.svt.domain.port.DistancePort;
+import com.pcunha.svt.infrastructure.api.HaversineDistanceAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class GameConfig {
+
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+
+    @Bean
+    public ActionHandler actionHandler(Random random) {
+        return new ActionHandler(random);
+    }
+
+    @Bean
+    public ConditionEvaluator conditionEvaluator() {
+        return new ConditionEvaluator();
+    }
+
+    @Bean
+    public EventProcessor eventProcessor(Random random) {
+        return new EventProcessor(random);
+    }
+
+    @Bean
+    public TurnProcessor turnProcessor(ActionHandler actionHandler, ConditionEvaluator conditionEvaluator, EventProcessor eventProcessor) {
+        return new TurnProcessor(actionHandler, conditionEvaluator, eventProcessor);
+    }
+
+    @Bean
+    public DistancePort distancePort() {
+        return new HaversineDistanceAdapter();
+    }
+
+    @Bean
+    public GameEngine gameEngine(TurnProcessor turnProcessor, DistancePort distancePort) {
+        return new GameEngine(turnProcessor, distancePort);
+    }
+}
